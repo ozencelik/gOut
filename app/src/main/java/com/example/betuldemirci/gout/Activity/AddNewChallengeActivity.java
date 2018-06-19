944a5327a8be4327d7c866043698cedd15f1a294
ozencelik <ozenc.celik@ceng.deu.edu.tr>
2018-06-19 16:54:21 +0300

    Betül Dil Seçeneği Ekleyecek

---
+++ app/src/main/java/com/example/betuldemirci/gout/Activity/AddNewChallengeActivity.java
@@ -0,0 +1,38 @@
+package com.example.betuldemirci.gout.Activity;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import com.example.betuldemirci.gout.R;
+
+public class AddNewChallengeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_new_challenge);
+
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
+}
--- app/src/main/java/com/example/betuldemirci/gout/Pedometer/SensorRestarterBroadcastReceiver.java
+++ app/src/main/java/com/example/betuldemirci/gout/Pedometer/SensorRestarterBroadcastReceiver.java
@@ -4,6 +4,9 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.util.Log;
+import android.widget.Toast;
+
+import com.example.betuldemirci.gout.Activity.MainActivity;
 
 /**
  * Created by fabio on 24/01/2016.
@@ -12,6 +15,8 @@ public class SensorRestarterBroadcastReceiver extends BroadcastReceiver {
 
     @Override
     public void onReceive(Context context, Intent intent) {
+
+        Toast.makeText(context, "Broadcast", Toast.LENGTH_SHORT).show();
         Log.i(SensorRestarterBroadcastReceiver.class.getSimpleName(), "Service Stops! Oooooooooooooppppssssss!!!!");
 
         context.startService(new Intent(context, SensorService.class));
